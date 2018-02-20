a0eb90657bf9ef4e17f3bfbebff55eb0a430fa55
Shinmera <shinmera@tymoon.eu>
2018-02-20 13:53:30 +0100

    Minor

--- documentation.lisp
+++ documentation.lisp
@@ -7,57 +7,57 @@
 (in-package #:org.shirakumo.documentation-utils)
 
 (docs:define-docs
-  (defvar *documentation-tests*
+  (variable *documentation-tests*
     "Holds an alist of documentation types to test functions.
 
 The function should take one argument, the specifier, and
 return non-NIL if the symbol is bound for the given type.")
 
-  (defun documentation-test
+  (function documentation-test
     "Access the documentation test function for the given type.
 
 See *DOCUMENTATION-TESTS*")
   
-  (defun remove-documentation-test
+  (function remove-documentation-test
     "Remove the documentation test function for the given type.
 
 See *DOCUMENTATION-TESTS*")
 
-  (defmacro define-documentation-test
+  (function define-documentation-test
     "Shorthand to define a documentation test function.
 
 See *DOCUMENTATION-TESTS*")
 
-  (defvar *documentation-translators*
+  (variable *documentation-translators*
     "Holds an alist of documentation types to translator functions.
 
 The function should take one argument, the specifier expression, and
 return a documentation form suitable to access the documentation
 for the given type.")
 
-  (defun documentation-translator
+  (function documentation-translator
     "Access the documentation translator function for the given type.
 
 See *DOCUMENTATION-TRANSLATORS*")
 
-  (defun remove-documentation-translator
+  (function remove-documentation-translator
     "Remove the documentation translator function for the given type.
 
 See *DOCUMENTATION-TRANSLATORS*")
 
-  (defmacro define-documentation-translator
+  (function define-documentation-translator
     "Shorthand to define a documentation translator function.
 
 See *DOCUMENTATION-TRANSLATORS*")
 
-  (defmacro define-documentation-alias
+  (function define-documentation-alias
     "Shorthand to define an alias to a translator.
 
 This simply sets a delegating function that refers to the given type.
 
 See *DOCUMENTATION-TRANSLATORS*")
 
-  (defun check
+  (function check
     "Checks whether all symbols have documentation for all known types.
 
 If documentation is not set for a given symbol and type combination, a
@@ -65,7 +65,7 @@ warning is signalled.
 
 See *DOCUMENTATION-TESTS*")
 
-  (defmacro define-docs
+  (function define-docs
     "Allows you to comfortably and easily set the documentation for your library.
 
 Each expression in the body can either take a two or many argument structure.
